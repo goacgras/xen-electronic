eb66abf9d750b884ad47022882cf7c43be5ca445
Reza Ghifary <fachrezaghifary@gmail.com>
2021-03-17 02:50:55 +0700

    add swagger

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,17 @@ import "reflect-metadata";
 import express, { Application } from "express";
 import morgan from "morgan";
 
+import swaggerUi from "swagger-ui-express";
+import * as swaggerDoc from "../swagger.json";
+
 const app: Application = express();
 
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(morgan("dev"));
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
 app.listen(port, () => {
     console.log(`server running at port ${port}`);
