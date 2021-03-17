6ed33c4216ee4fd9b1c0875595a9404a2a085924
Reza Ghifary <fachrezaghifary@gmail.com>
2021-03-17 09:55:43 +0700

    add order and product routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,10 @@ import morgan from "morgan";
 
 import swaggerUi from "swagger-ui-express";
 import * as swaggerDoc from "../swagger.json";
+import { createConnection } from "typeorm";
+
+import productRoutes from "./routes/product";
+import orderRoutes from "./routes/order";
 
 const app: Application = express();
 
@@ -13,7 +17,15 @@ app.use(express.json());
 app.use(morgan("dev"));
 
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
+app.use("/api/product", productRoutes);
+app.use("/api/order", orderRoutes);
 
-app.listen(port, () => {
+app.listen(port, async () => {
     console.log(`server running at port ${port}`);
+    try {
+        await createConnection();
+        console.log("Database connected");
+    } catch (err) {
+        console.log(err);
+    }
 });
---
+++ server/src/routes/product.ts
@@ -0,0 +1,46 @@
+import { Request, Response, Router } from "express";
+import { Product } from "../entities/Product";
+
+const getProduct = async (req: Request, res: Response) => {
+    const category: string = req.params.category;
+
+    try {
+        const products = await Product.find({
+            where: {
+                category,
+            },
+            order: {
+                createdAt: "DESC",
+            },
+        });
+
+        return res.json(products);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ error: "something went wrong" });
+    }
+};
+
+const addProduct = async (req: Request, res: Response) => {
+    const { name, category, price } = req.body;
+
+    try {
+        const newProduct = new Product({
+            name,
+            category,
+            price,
+        });
+
+        await newProduct.save();
+        return res.json(newProduct);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ error: "something went wrong" });
+    }
+};
+
+const router = Router();
+router.get("/:category", getProduct);
+router.post("/", addProduct);
+
+export default router;
